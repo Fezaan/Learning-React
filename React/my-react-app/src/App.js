e1c8479b1f1db1ed6d6db3acc840c48f69dc28dc
Fezaan <110244796+Fezaan@users.noreply.github.com>
2023-06-01 15:38:09 +0530

    Components Practice

--- React/my-react-app/src/App.js
+++ React/my-react-app/src/App.js
@@ -1,41 +1,11 @@
 import "./App.css";
+import Logic from "./components/Logic";
 
-const date = new Date();
-const time = date.getHours();
-
-const obj={
-  color: "blue",
-  fontSize: "40px",
-  border: "1px dotted green"
-}
 
 export default function List() {
-  if (time >= 5 && time < 11) {
-    obj.color="orange";
-    return (
-      <>
-        <h1 style={obj}>Good Morning</h1>
-      </>
-    );
-  } else if (time >= 11 && time < 16) {
-    obj.color="yellow";
-    return (
-      <>
-        <h1 style={obj}>Good Afternoon</h1>
-      </>
-    );
-  } else if (time >= 16 && time < 19) {
-    obj.color= "red";
-    return (
-      <>
-        <h1 style={obj}>Good Evening</h1>
-      </>
-    );
-  } else {
-    return (
-      <>
-        <h1 style={obj}>Good Night</h1>
-      </>
-    );
-  }
+  return (
+    <>
+      <Logic />
+    </>
+  );
 }
---
+++ React/my-react-app/src/components/Logic.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const date = new Date();
+const time = date.getHours();
+let greeting="";
+
+const obj = {
+  color: "blue",
+  fontSize: "40px",
+  border: "1px dotted green",
+};
+
+export default function Logic(){
+    if (time >= 5 && time < 11) {
+        obj.color="orange";
+        greeting="Good Morning";
+    } else if (time >= 11 && time < 16) {
+        obj.color="yellow";
+        greeting="Good Afternoon";
+    } else if (time >= 16 && time < 19) {
+        obj.color= "red";
+        greeting="Good Evening";
+    } else {
+        obj.greeting="Good Night";
+    }
+    return(<><h1 className="heading" style={obj}>{greeting}</h1></>)
+}
